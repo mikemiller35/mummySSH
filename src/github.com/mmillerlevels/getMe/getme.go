682bd675d5d39deda7a4f52653db551e0081ad7b
Mike Miller <39773669+mmillerlevels@users.noreply.github.com>
2019-02-13 10:21:48 -0700

    Update getme.go
    Adding the basic http request to the Vault.  Will need to modify this obviously

--- src/github.com/mmillerlevels/getMe/getme.go
+++ src/github.com/mmillerlevels/getMe/getme.go
@@ -1,1 +1,26 @@
-Placeholder
+package getme
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+)
+
+func main() {
+
+	url := "https://SECRETS.SHH"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("X-Vault-Token", "NOTYOURAPIKEY")
+	req.Header.Add("cache-control", "no-cache")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+
+}
